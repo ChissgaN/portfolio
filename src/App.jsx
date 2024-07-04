3068058557f23e7904d9ad01c4bb75394b945b95
ChissgaN <coderchissgan@gmail.com>
2024-07-04 13:07:32 -0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='bg-[#191919]'>
+      <Navbar/>
+    </div>
   )
 }
 
